bbf4ff87c1c2ddd3754698391d99310037ccc78d
Carlesneri <carlesneri@hotmail.com>
2020-01-29 07:34:43 +0100

    Prof

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Nav from '../Nav/Nav';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Switch, Route, HashRouter } from 'react-router-dom';
 import OddsportalDrop from '../OddsportalDrop/OddsportalDrop';
 import Drop from '../Drop/Drop'
 import Home from '../Home/Home';
@@ -9,14 +9,14 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
-      <BrowserRouter >
+      <HashRouter >
           <Nav />
           <Switch>
             <Route path='/' exact component={Home} />
             <Route path='/comparator' exact component={OddsportalDrop} />
             <Route path='/drop' exact component={Drop} />
           </Switch>
-      </BrowserRouter>
+      </HashRouter>
     );
   }
 }
